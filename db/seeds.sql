d81e6020104186d320fc57d27c52810b3fd9341a
amber phillips <amberphillips@ambers-MBP.DunnAddison>
2018-05-06 13:45:20 -0500

    ed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+update burgers set burger_name = "burger" WHERE id = 1 
+update burgers set burger_name = "burger" WHERE id = 2
+update burgers set burger_name = "burger" WHERE id = 3 
---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var mysql = require("body-parser");
+
+// Create express app instance.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
