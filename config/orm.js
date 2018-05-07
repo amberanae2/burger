2e080e7777a3641701fb536faae46f151d7639ca
amber phillips <amberphillips@ambers-MacBook-Pro.local>
2018-05-07 12:59:48 -0500

    fire

--- config/orm.js
+++ config/orm.js
@@ -1,48 +1,14 @@
 var connection = require("../config/connection.js");
 
-function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
-  }
-  
-  // Helper function to convert object key/value pairs to SQL syntax
-  function objToSql(ob) {
-    var arr = [];
-  
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
-        arr.push(key + "=" + value);
-      }
-    }
-  
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
-  }
-  
-  // Object for all our SQL statement functions.
   var orm = {
-    select: function(tableInput, colToSearch, valOfCol) {
+    Select: function(tableInput, colToSearch, valOfCol) {
       var queryString = "SELECT * FROM ?? WHERE ?? = ?";
       connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
         if (err) throw err;
         console.log(PartyName);
       });
     },
-    select: function(whatToSelect, table, orderCol) {
+    Select: function(whatToSelect, table, orderCol) {
         var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
         console.log(clientName);
         connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-update burgers set burger_name = "burger" WHERE id = 1 
-update burgers set burger_name = "burger" WHERE id = 2
-update burgers set burger_name = "burger" WHERE id = 3 
+update burgers set burger_name = "triple cheese burger" WHERE id = 1 
+update burgers set burger_name = "bacon cheeseburger" WHERE id = 2
+update burgers set burger_name = " veggie burger" WHERE id = 3 
